ec25e28e7f72e5e3b5f7e9140c3513233681a679
niki-shan <nikitawaghmare96@gmail.com>
2023-06-26 21:53:07 +0530

    reen

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+{/* <a class="nav-link" href="javascript:;">PORTFOLIO</a> */ portfol}
+
+
+
+
+
+
+
+/*	<div class="row">
+					<div class="col-md-1 col-xs-3"><a href="javascript:;"><i class="fa-regular fa-heart fa=3x"></i></a></div>
+					<div class="col-md-3 col-xs-6">
+						<h3>Passion</h3>
+						<p>Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+					</div>
+						
+					<div class="col-md-1 col-xs-3"><a href="javascript:;"><i class="fa-regular fa-lightbulb"></i></a></div>
+					<div class="col-md-3 col-xs-9">
+						<h3>Creativity</h3>
+						<p>Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+					</div>
+						<div class="col-md-1 col-xs-3"><a href="javascript:;"><i class="fa-regular fa-star fa-3x"></i></a></div>
+					<div class="col-md-3 col-xs-9">
+						<h3>Quality</h3>
+						<p>Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+					</div>
+
+					</div>
+				
